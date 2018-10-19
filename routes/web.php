1ad2849a41a78aae6cc6f963eef6a6de472163d6
fixable11 <fixable11@gmail.com>
2018-10-19 21:49:14 +0300

    Added login/register functionality

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,22 @@ Route::get('/post/{slug}', 'HomeController@show')->name('post.show');
 Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('/register', 'AuthController@registerForm')->name('register');
+    Route::post('/register', 'AuthController@register');
+    Route::get('/login', 'AuthController@loginForm')->name('login');
+    Route::post('/login', 'AuthController@login');
+});
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/logout', 'AuthController@logout')->name('logout');
+});
+
+Route::group([
+    'prefix' => 'admin', 
+    'namespace' => 'Admin', 
+    'middleware' => 'admin'
+], function () {
 
     Route::get('/', 'DashboardController@index');
     Route::resource('/categories', 'CategoriesController');
